7412db68195ef1b0cb150bc74fdb829d14db70ce
Kendall Howard <69732946+kenxh@users.noreply.github.com>
2022-04-06 19:46:34 -0400

    Changing help again

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ const port = args.port || process.env.PORT || 5555
 const debug = args.debug
 const log = args.log
 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
 if (args.help) {
-    console.log('Return this message and exit.');
+    console.log(help);
     exit(0);
 }
 
